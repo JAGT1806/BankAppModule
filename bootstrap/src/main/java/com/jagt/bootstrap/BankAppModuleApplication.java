45ba7925c0abca0743dd6dfa610aa39810095208
JAGT1806 <jagt1806@gmail.com>
2025-06-25 21:43:56 -0500

    chore: add module bootstrap and change Spring Boot application to this module

--- bootstrap/src/main/java/com/jagt/bootstrap/BankAppModuleApplication.java
+++ bootstrap/src/main/java/com/jagt/bootstrap/BankAppModuleApplication.java
@@ -1,4 +1,4 @@
-package com.jagt.bankappmodule;
+package com.jagt.bootstrap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
