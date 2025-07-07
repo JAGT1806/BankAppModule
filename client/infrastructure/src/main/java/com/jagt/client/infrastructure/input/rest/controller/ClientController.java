0dcb6473ce14f590d9812450ed8c510fce9b5c0e
JAGT1806 <jagt1806@gmail.com>
2025-07-06 21:39:03 -0500

    refactor: add docs to ClientController

--- client/infrastructure/src/main/java/com/jagt/client/infrastructure/input/rest/controller/ClientController.java
+++ client/infrastructure/src/main/java/com/jagt/client/infrastructure/input/rest/controller/ClientController.java
@@ -5,6 +5,7 @@ import com.jagt.client.application.ports.input.DeleteClientUseCase;
 import com.jagt.client.application.ports.input.GetClientUseCase;
 import com.jagt.client.application.ports.input.UpdateClientUseCase;
 import com.jagt.client.domain.model.Client;
+import com.jagt.client.infrastructure.input.rest.controller.docs.ClientControllerApi;
 import com.jagt.client.infrastructure.input.rest.mapper.ClientRestMapper;
 import com.jagt.client.infrastructure.input.rest.request.ClientCreateRequest;
 import com.jagt.client.infrastructure.input.rest.request.ClientUpdateRequest;
@@ -13,42 +14,43 @@ import com.jagt.common.domain.model.Pagination;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/clients")
 @RequiredArgsConstructor
-public class ClientController {
+public class ClientController implements ClientControllerApi {
     private final GetClientUseCase getClientUseCase;
     private final CreateClientUseCase createClientUseCase;
     private final UpdateClientUseCase updateClientUseCase;
     private final DeleteClientUseCase deleteClientUseCase;
     private final ClientRestMapper mapper;
 
-    @GetMapping
-    public Pagination<Client> getClients(@RequestParam(required = false, defaultValue = "0") int offset, @RequestParam(required = false, defaultValue = "12") int limit) {
+    @Override
+    public Pagination<Client> getClients(int offset, int limit) {
         return getClientUseCase.execute(mapper.toQuery(offset, limit));
     }
 
-    @GetMapping("/{id}")
-    public ClientResponse getClient(@PathVariable Long id) {
+    @Override
+    public ClientResponse getClient(Long id) {
         return mapper.toResponse(getClientUseCase.execute(id));
     }
 
-    @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public ClientResponse createClient(@Valid @RequestBody ClientCreateRequest request) {
-        return mapper.toResponse(createClientUseCase.execute(mapper.toCommand(request)));
+    @Override
+    public ResponseEntity<ClientResponse> createClient(ClientCreateRequest request) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(mapper.toResponse(createClientUseCase.execute(mapper.toCommand(request))));
     }
 
-    @PutMapping("/{id}")
-    public ClientResponse updateClient(@PathVariable Long id, @Valid @RequestBody ClientUpdateRequest request) {
+    @Override
+    public ClientResponse updateClient(Long id, ClientUpdateRequest request) {
         return mapper.toResponse(updateClientUseCase.execute(mapper.toCommand(id, request)));
     }
 
-    @DeleteMapping("/{id}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteClient(@PathVariable Long id) {
+    @Override
+    public ResponseEntity<Void> deleteClient(@PathVariable Long id) {
         deleteClientUseCase.execute(id);
+        return ResponseEntity.noContent().build();
     }
 }
